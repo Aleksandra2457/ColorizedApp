38ced14b5bde2e72caf5d22bc02b20bdf01f5c50
Aleksandra Moiseenko <lesovskaya.aleks@mail.ru>
2022-03-05 20:49:40 +0300

    Added changes to constraints. Refactoring.

--- ColorizedApp/RedGreenBlueViewController.swift
+++ ColorizedApp/RedGreenBlueViewController.swift
@@ -23,18 +23,27 @@ class RedGreenBlueViewController: UIViewController {
     // MARK: - Life Cycles Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        redGreenBlueView.layer.cornerRadius = 10
+        redGreenBlueView.layer.cornerRadius = 15
+        updateUserInterface()
     }
     
     // MARK: - Public Methods
     @IBAction func sliderValueChanged() {
+        updateUserInterface()
+    }
+    
+    // MARK: - Private Methods
+    private func updateUserInterface() {
         redColorLabel.text = String(format: "%.2f", redColorSlider.value)
         greenColorLabel.text = String(format: "%.2f", greenColorSlider.value)
         blueColorLabel.text = String(format: "%.2f", blueColorSlider.value)
+        let redColor = CGFloat(redColorSlider.value)
+        let greenColor = CGFloat(greenColorSlider.value)
+        let blueColor = CGFloat(blueColorSlider.value)
         redGreenBlueView.backgroundColor = UIColor(
-            red: CGFloat(redColorSlider.value),
-            green: CGFloat(greenColorSlider.value),
-            blue: CGFloat(blueColorSlider.value),
+            red: redColor,
+            green: greenColor,
+            blue: blueColor,
             alpha: 1
         )
     }
