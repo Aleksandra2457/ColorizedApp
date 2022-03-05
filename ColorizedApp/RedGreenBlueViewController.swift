d56275f2b84d39bc0bc8b618fdbbe731e7a4a764
Aleksandra Moiseenko <lesovskaya.aleks@mail.ru>
2022-03-05 20:02:37 +0300

    Implement the logics.

---
+++ ColorizedApp/RedGreenBlueViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  ColorizedApp
+//
+//  Created by Александра Лесовская on 05.03.2022.
+//
+
+import UIKit
+
+class RedGreenBlueViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    @IBOutlet var redGreenBlueView: UIView!
+    
+    @IBOutlet var redColorLabel: UILabel!
+    @IBOutlet var greenColorLabel: UILabel!
+    @IBOutlet var blueColorLabel: UILabel!
+    
+    @IBOutlet var redColorSlider: UISlider!
+    @IBOutlet var greenColorSlider: UISlider!
+    @IBOutlet var blueColorSlider: UISlider!
+    
+    // MARK: - Life Cycles Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        redGreenBlueView.layer.cornerRadius = 10
+    }
+    
+    // MARK: - Public Methods
+    @IBAction func sliderValueChanged() {
+        redColorLabel.text = String(format: "%.1f", redColorSlider.value)
+        greenColorLabel.text = String(format: "%.1f", greenColorSlider.value)
+        blueColorLabel.text = String(format: "%.1f", blueColorSlider.value)
+        redGreenBlueView.backgroundColor = UIColor(
+            red: CGFloat(redColorSlider.value),
+            green: CGFloat(greenColorSlider.value),
+            blue: CGFloat(blueColorSlider.value),
+            alpha: 1
+        )
+    }
+    
+}
+
